62fbae24c02c1cf811c6ef352cdcdf7992e308d6
DG Costa <costadiegus@gmail.com>
2017-11-24 13:21:53 -0200

    Finalização da Aula 07

--- src/app/cursos/cursos.component.ts
+++ src/app/cursos/cursos.component.ts
@@ -1,4 +1,6 @@
+import { CursosService } from './cursos.service';
 import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-cursos',
@@ -8,10 +10,11 @@ import { Component, OnInit } from '@angular/core';
 export class CursosComponent implements OnInit {
 
   nomePortal: string;
-  cursos: string[] = ['Java', 'ExtJS', 'Angular'];
+  cursos: string[];
 
-  constructor() { 
+  constructor(private cursosService : CursosService) { 
     this.nomePortal = 'http://loiane.training';
+    this.cursos = this.cursosService.getCursos();
   }
 
   ngOnInit() {
