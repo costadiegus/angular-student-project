f731033b812bbf7e44b6dfa767a42bd3373908b5
DG Costa <costadiegus@gmail.com>
2019-06-09 08:07:57 -0300

    Angular 8 Update (new + src)

---
+++ meu-primeiro/src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
+import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
+import { CursosModule } from './cursos/cursos.module';
+import { DataBindingComponent } from './data-binding/data-binding.component';
+import { MeuFormModule } from './meu-form/meu-form.module';
+import { InputPropertyComponent } from './input-property/input-property.component';
+import { OutputPropertyComponent } from './output-property/output-property.component';
+import { CicloComponent } from './ciclo/ciclo.component';
+import { DiretivaNgifComponent } from './diretiva-ngif/diretiva-ngif.component';
+import { MeuFormComponent } from './meu-form/meu-form.component';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MeuPrimeiroComponent,
+    MeuPrimeiro2Component,
+    DataBindingComponent,
+    InputPropertyComponent,
+    OutputPropertyComponent,
+    CicloComponent,
+    DiretivaNgifComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    CursosModule,
+    MeuFormModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ meu-primeiro/src/app/diretiva-ngif/diretiva-ngif.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-diretiva-ngif',
+  templateUrl: './diretiva-ngif.component.html',
+  styleUrls: ['./diretiva-ngif.component.css']
+})
+export class DiretivaNgifComponent implements OnInit {
+
+  cursos: string[] = ['Angular 5', 'ExtJS', 'React JS', 'Ionic'];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
